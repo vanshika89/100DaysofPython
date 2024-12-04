0999d3b8f5bb2de909d396f9d67fb44aece6be75
Vanshika Dharwal <Vanshika.18CS153@dpgitm.ac.in>
2024-12-04 20:49:44 +0530

    Merge Sort in Python

--- QuickSort.py
+++ QuickSort.py
@@ -1,21 +1,19 @@
-# O(n log n) in all cases, including the best, average, and worst case
 import random
 
 
 def merge_sort(array):
-    if len(array) <= 1: # Base case: arrays of length 1 or less are already sorted
+    if len(array) <= 1:  # already sorted
         return
 
-    # Spliting the array into two halves
     mid = len(array) // 2
     left_array = array[:mid]
     right_array = array[mid:]
 
-    # Recursively sorting the two halves
+    # calling merge sort on each array
     merge_sort(left_array)
     merge_sort(right_array)
 
-    # Merging the sorted arrays into the original array
+    # merging the sorted arrays
     i = j = k = 0
     while i < len(left_array) and j < len(right_array):
         if left_array[i] < right_array[j]:
@@ -26,18 +24,18 @@ def merge_sort(array):
             j += 1
         k += 1
 
-    # Copy any remaining elements
     while i < len(left_array):
         array[k] = left_array[i]
         i += 1
         k += 1
+
     while j < len(right_array):
         array[k] = right_array[j]
         j += 1
         k += 1
 
 
-input_array = [random.randint(1, 100) for _ in range(10)]
-print("Random input array:", input_array)
-merge_sort(input_array)
-print("Sorted array", input_array)
+input_array = [random.randint(1, 100) for _ in range(0, 10)]
+print('Input array :', input_array)
+sorted_array = merge_sort(input_array)
+print('Sorted array', input_array)
