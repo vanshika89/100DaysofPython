c83fcc68c635ed276acfa52bd4ad187c88939590
Vanshika Dharwal <Vanshika.18CS153@dpgitm.ac.in>
2025-03-01 00:58:58 +0530

    Day5: Caesar Cipher to encrypt and decrypt

--- Day5_Caesar_Cipher.py
+++ Day5_Caesar_Cipher.py
@@ -4,21 +4,28 @@ alphabet = list(string.ascii_lowercase)
 
 
 def encrypt_decrypt(user_prefrence, original_text, shift_amount):
-    if user_prefrence == 'encode:':
-        pass
-    elif user_prefrence == 'decode':
-        pass
-    else:
-        print("Wrong input please try again..")
+    cipher_text = ''
+    for letter in original_text:
+        if ' ' in letter:
+            cipher_text += ' '
+        if user_prefrence == 'decode':
+            shift_amount *= -1
+
+        new_location = alphabet.index(letter) + shift_amount
+        new_location %= len(alphabet)
+        cipher_text += alphabet[new_location]
+    print(f"Here is the {user_prefrence} result: {cipher_text}")
 
 
-game = True
-while game:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = input("Type the shift number:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+if direction == 'encode' or 'decode':
     encrypt_decrypt(direction, text, shift)
-    user_message = input("Type 'yes' if you want to go again else type 'no':\n").lower()
-    if user_message == 'no':
-        print('Goodbye')
-        game = False
+else:
+    print('Choose from the given option')
+
+# user_message = input("Type 'yes' if you want to go again else type 'no':\n").lower()
+# if user_message == 'no':
+#     print('Goodbye')
+#     game = False
