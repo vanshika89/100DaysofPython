e29609cc67cb3b4a9c93f509c86e3184a7f64e81
Vanshika Dharwal <Vanshika.18CS153@dpgitm.ac.in>
2025-03-09 00:50:49 +0530

    Day8: Calculator

--- Day8_Calculator.py
+++ Day8_Calculator.py
@@ -20,19 +20,29 @@ operations = {
     "*": multiply,
     "/": divide,
 }
-should_accumulate = True
-number1 = float(input("What's the first number?: "))
-while should_accumulate:
-    for symbol in operations:
-        print(symbol)
-    operation = input("Pick an operation: ")
-    if operation in operations:
-        number2 = float(input("What's the next number?: "))
-        answer = operations[operation](number1, number2)
-        print(f"{number1} {operation}{number2} = {answer}")
-    else:
-        print("Choose from given operations: ")
 
-    choice = input(f"Do you want to continue with calculating with {answer}? tyoe 'y' for yes and 'n' for no:" )
-    if choice=='y':
-        number1 = answer
+
+def calculator():
+    should_accumulate = True
+    number1 = float(input("What's the first number?: "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Pick an operation: ")
+        if operation in operations:
+            number2 = float(input("What's the next number?: "))
+            answer = operations[operation](number1, number2)
+            print(f"{number1} {operation}{number2} = {answer}")
+        else:
+            print("Choose from given operations: ")
+
+        choice = input(f"Do you want to continue with calculating with {answer}? tyoe 'y' for yes and 'n' for no:")
+        if choice == 'y':
+            number1 = answer
+        else:
+            should_accumulate = False
+            print('\n' * 20)
+            calculator()
+
+
+calculator()
